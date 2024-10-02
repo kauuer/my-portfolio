c562ebf74124f1717f81da6b0d29c45ba8102644
kaue rodrigues <iggcode@gmail.com>
2024-10-01 23:00:58 -0300

    feat:hero component .ui

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -13,7 +13,14 @@ const Hero = () => {
     backgroundColor: theme.palette.primary.main,
     height: "100vh",
     display: "flex",
-    alignItems: "center"
+    alignItems: "center",
+    [theme.breakpoints.up('xs')]: {
+      paddingTop: "100px"
+    },
+    [theme.breakpoints.up('md')]: {
+      paddingTop: "0",
+    }
+
   }))
 
   const StyledImg = styled("img")(() => ({
